77649a0f58a40f102fc22c043cf239523a3c89f6
anji606 <anjireddyb98@gmail.com>
2025-07-12 07:16:04 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,15 +3,15 @@ import os
 class Config(object):
   API_ID = int(os.environ.get("API_ID", "29055333"))
   API_HASH = os.environ.get("API_HASH", "a6d154242eaef80a163bf5d0a7763882")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "7795414140:AAHPYxU-Yc7AJ3KcR-zWZmqFOYd4C_mNItQ")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "Cartoonworldtelugu_india09_bot")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "8178307547:AAHx8tkXwi79zmWY3ONk5GOaeV61Dm1i_aM")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "Cartoonworldtelugu_india_bot")
   DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002526508320"))
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
   BOT_OWNER = int(os.environ.get("BOT_OWNER", "6828129421"))
   DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://anjireddyb98:3qiaVJINXarqFNKp@cluster0.kohnpcd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002024495069")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002556802708"))
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002630777690"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
@@ -31,11 +31,11 @@ This is a Permanent FileStore Bot Created by Vamsi. This Bot is for Persnol use 
 ╰──────[ 😎 ]───────────⍟
 """
   ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/KingVj01)
+🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/Vamsi_2008_vamsi)
  
  I am Super noob Please Support My Hard Work.
 
-[Donate Me](https://t.me/KingVj01)
+[Donate Me](https://t.me/KVT_Telugu)
 """
   HOME_TEXT = """
 Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
